d54665142f64903bd2aaa8a0a0713b562063dcee
besit06 <behemoth2054@gmail.com>
2024-11-30 15:47:11 +0100

    goit-react-hw-06

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,45 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
+
 const persistConfig = {
-  key: 'contacts',
+  key: 'root',
+  version: 1,
   storage,
-  whitelist: ['items'], 
 };
 
+
 const rootReducer = combineReducers({
-  contacts: persistReducer(persistConfig, contactsReducer),
+  contacts: contactsReducer,
   filters: filtersReducer,
 });
 
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
 
 export const persistor = persistStore(store);
